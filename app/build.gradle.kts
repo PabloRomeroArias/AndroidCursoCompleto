2918a75a28272b6322644c7f77bb1e9450f28fcf
Pablo Romero Arias <pablo.romeroarias.main@gmail.com>
2023-09-29 09:02:44 +0200

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.devepoler.androidcursocompleto"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.devepoler.androidcursocompleto"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
